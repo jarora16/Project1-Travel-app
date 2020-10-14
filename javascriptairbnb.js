b9c81c04b62afb8a943a275bfdaf9731818dac62
jarora16 <71145914+jarora16@users.noreply.github.com>
2020-10-14 11:58:49 -0400

    Delete javascriptairbnb.js

--- javascriptairbnb.js
+++
@@ -1,6 +0,0 @@
-// apikey = 282ee419b4500a6eef79a6ccc4463ff5
-fetch(
-  "https://api.nomics.com/v1/exchange-rates?key=282ee419b4500a6eef79a6ccc4463ff5"
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
